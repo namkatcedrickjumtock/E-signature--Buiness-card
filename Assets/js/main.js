377c1f504aca55221b19879e4d64fbfe4eb12bc7
Namkat Cedrick j <namkatcedrickjumtock3@gmail.com>
2022-05-19 16:16:41 +0100

    wokring e-signature card

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,13 +1,44 @@
-// helper functions
- let getElementId = (id) => document.getElementById(id)
- let getElementName = (name) => document.getElementsByClassName(name)
-  let getELementByQuery = (query) => document.querySelectorAll(query)
+// // helper functions
+// function getElementId(id) { return document.getElementById(id) }
+// function getElementName(name) { return document.getElementsByClassName(name) }
+// function getELementByQuery(query) { return document.querySelectorAll(query) }
 
 
 
-// function to get elements by Id from Dom
+// on window load
 window.onload = (event) => {
-  body.addEventListener('click', (even) =>{
-getElementId('body')
-  })
-};
+
+
+  // const toggle = document.querySelector('customSwitch1');
+
+  function changeTheme() {
+    let element = document.body;
+    element.classList.toggle("dark-mode")
+  }
+
+
+}
+
+
+function getTime() {
+  let Datime = new Datime()
+}
+
+
+
+// let isChanged = false;
+// let target = getElementId('card')
+// let button = getElementId('customSwitch1')
+
+// button.addEventListener('click', (event) => {
+//   if (isChanged) {
+//     target.classList.remove("bg-white")
+//     target.classList.add("bg-dark")
+//     isChanged = true
+//   } else {
+//     target.classList.remove("bg-dark")
+//     target.classList.add("bg-white")
+//     isChanged = false
+
+//   }
+// })
